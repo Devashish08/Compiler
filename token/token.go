0082e9e14642543aa6d2d93984b40f524f77dcbb
Devashish08 <devashish.cs025@gmail.com>
2024-03-08 16:03:41 +0530

    implement basic Lexer

---
+++ token/token.go
@@ -0,0 +1,50 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+/*
+LookupIdent checks the keywords table to see whether the given identifier is in fact a keyword.
+If it is, it returns the keyword’s TokenType constant. If it isn’t, we just get back token.IDENT,
+which is the TokenType for all user-defined identifiers.
+*/
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
